d24777befaaf8a2a00fe3ff58e7ce1e45b44a309
ruslanraindrop <inventmeplease@gmail.com>
2023-02-27 19:08:57 +0300

    Fixed bugs and added parametrize - Fixed assert in product_page - Added parametrize for test_guest_can_add_product_to_cart

--- pages/product_page.py
+++ pages/product_page.py
@@ -14,11 +14,13 @@ class ProductPage(BasePage):
     def should_be_correct_message(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         added_product_message = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_MESSAGE).text
-        assert product_name in added_product_message, \
+        print(f"product_name={product_name}, added_name={added_product_message}")
+        assert product_name == added_product_message, \
             f"This message is wrong: added {product_name}, not a {added_product_message}"
 
     def should_be_correct_price(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         added_product_price = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_PRICE).text
-        assert product_price in added_product_price, \
+        print(f"product_price={product_price}, added_price={added_product_price}")
+        assert product_price == added_product_price, \
             f"This price is wrong: added product for a price {product_price}, not a {added_product_price}"
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,9 +1,12 @@
+import pytest
 from pages.product_page import ProductPage
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
-
-def test_guest_can_add_product_to_cart(browser):
+@pytest.mark.parametrize('offer_id', ["0", "1", "2", "3", "4", "5", "6",
+                                      pytest.param("7", marks=pytest.mark.xfail),
+                                      "8", "9"])
+def test_guest_can_add_product_to_cart(browser, offer_id):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_id}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
