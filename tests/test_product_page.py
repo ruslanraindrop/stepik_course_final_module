b0140b53ba123fcb721568bd449a34e6762fba4e
ruslanraindrop <inventmeplease@gmail.com>
2023-04-23 15:22:24 +0300

    - Update pytest.ini with marks - Made imports more readable

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 import time
+
 from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
@@ -24,6 +25,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
@@ -32,6 +34,7 @@ class TestUserAddToBasketFromProductPage:
         page.should_be_correct_product_added()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('offer_id', ["?promo=offer0", "?promo=offer1", "?promo=offer2",
                                       "?promo=offer3", "?promo=offer4", "?promo=offer5",
                                       "?promo=offer6",
@@ -79,6 +82,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -89,6 +93,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
